d2adc1bf3f5e877389ada408fa1da402eeb7029b
Dipen Bagia <dipen.bagia@gmail.com>
2018-02-11 00:41:38 +0530

    formatted code

--- src/generator/index.js
+++ src/generator/index.js
@@ -8,23 +8,23 @@ let formData = {}
 
 // addOnChange:: (Object) -> (JSON) -> []
 const addOnChange = R.curry((fields, schema) =>
-schema.map(field => {
-  field.onChange = event => {
-    const value = event.target.value
-    fields[field.name] = value
-  }
-  return field
-}))
+  schema.map(field => {
+    field.onChange = event => {
+      const value = event.target.value
+      fields[field.name] = value
+    }
+    return field
+  }))
 
 // inputs:: (JSON) -> []
 const inputs = schema =>
-    schema
-    .map(processInputs)
-    .map(maybe =>
-      maybe.getOrElse(
-        <div className='my2'>This field is missing required properties</div>
-      )
+  schema
+  .map(processInputs)
+  .map(maybe =>
+    maybe.getOrElse(
+      <div className='my2'>This field is missing required properties</div>
     )
+  )
 
 // lists:: (JSON) -> []
 const lists = schema =>
@@ -35,12 +35,15 @@ const lists = schema =>
       : s
     )
 
+// log:: IMPURE a -> a
 const log = o => {
   console.log(o)
   return o
 }
 
+// filter:: (String) -> (JSON) -> []
 const filter = R.curry((type, schema) => schema.filter(s => s.type === type))
+
 const generateForm = R.pipe(addOnChange(formData), log, inputs, lists)
 
 export {generateForm, formData, inputs, lists, addOnChange}
