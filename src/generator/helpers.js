0adbf4a0a08e11e21c557eed0d21e1aa6705f01d
Dipen Bagia <dipen.bagia@gmail.com>
2018-02-25 00:32:58 +0530

    moved utility functions to helpers.js

--- src/generator/helpers.js
+++ src/generator/helpers.js
@@ -1,5 +1,30 @@
 import {fromPromise} from 'crocks/Async'
 import axios from 'axios'
+import curry from 'crocks/helpers/curry'
+import IO from 'crocks/IO'
+import propOr from 'crocks/helpers/propOr'
+
+// defaultProp:: String -> (String -> String)
+const defaultProp = propOr('string')
 
 export const asyncAxiosGet =
   fromPromise(axios.get)
+
+// getData:: String -> Async
+export const getData =
+  url => asyncAxiosGet(url)
+
+// toOptions:: [a] -> jsx
+export const toOptions = options =>
+  options
+  .map((option, i) =>
+    <option key={i} value={option.id}>{option.name}</option>
+  )
+
+export const $ = selector =>
+  IO.of(document.querySelectorAll(selector))
+
+// isSchemaItemOfType:: String -> a -> Boolean
+export const isSchemaItemOfType =
+  curry((type, schemaItem) =>
+    defaultProp('type', schemaItem) === type)
