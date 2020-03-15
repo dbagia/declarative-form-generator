9c47148e1035e9820372acbe4095b559648a0cc0
dipen bagia <dipen.bagia@klarna.com>
2020-03-15 18:58:08 +0100

    convert class to hook

--- src/generator/Lookup_Component.jsx
+++ src/generator/Lookup_Component.jsx
@@ -1,44 +1,32 @@
-import React from 'react'
-import {getData, toOptions} from './helpers'
+import React, { useState, useEffect } from 'react'
+import { getData, toOptions } from './helpers'
 
-export default class LookupComponent extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      options: []
-    }
-  }
+export default function LookupComponent(props) {
+  const [options, setOptions] = useState([])
 
-  componentDidMount () {
-    getData(this.props.field.lookupUrl)
-    .map(r => r.data)
-    .map(toOptions)
-    .fork(
-      err => console.log('err', err),
-      options => {
-        this.setState(
-          {options}
-        )
-      }
-    )
-  }
+  useEffect(() => {
+    getData(props.field.lookupUrl)
+      .map(r => r.data)
+      .map(toOptions)
+      .fork(
+        err => console.log('err', err), setOptions
+      )
+  })
 
-  render () {
-    const {field} = this.props
-    return (
-      <div className='my2'>
-        <label className='mr2 mb2'>
-          {field.label}
-        </label>
-        <select
-          required={field.required}
-          readOnly={field.readOnly}
-          onChange={field.onChange}
-          className='mb2'
-        >
-          {this.state.options}
-        </select>
-      </div>
-    )
-  }
+  return (
+    <div className='my2'>
+      <label className='mr2 mb2'>
+        {props.field.label}
+      </label>
+      <select
+        required={props.field.required}
+        readOnly={props.field.readOnly}
+        onChange={props.field.onChange}
+        className='mb2'
+      >
+        {options}
+      </select>
+    </div>
+  )
 }
+
